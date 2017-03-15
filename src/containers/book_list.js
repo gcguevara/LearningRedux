65301eb6ecf02be45fe2e76b799942d13881f706
gcguevara <garrettcguevara@gmail.com>
2017-03-15 14:31:21 -0700

    Added beginning implementations of redux.

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -7,7 +7,11 @@ class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">{book.title}
+        </li>
       );
     });
   }
@@ -28,4 +32,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+// promote BookList from a component to a container - it needs to know about
+// the new dispatch method selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
